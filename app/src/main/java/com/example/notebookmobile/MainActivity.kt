14222f013a8128cb326b7c17aebb195ca1c314f0
CarlosL62 <carlosalfredolopez96@gmail.com>
2025-02-17 23:59:43 -0600

    savepoint: text and code button basic function

--- app/src/main/java/com/example/notebookmobile/MainActivity.kt
+++ app/src/main/java/com/example/notebookmobile/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.notebookmobile
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.LinearLayout
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -17,31 +21,34 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            NotebookMobileTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+        setContentView(R.layout.activity_main)
+
+        val textInputButton = findViewById<Button>(R.id.textInputButton)
+        val codeInputButton = findViewById<Button>(R.id.codeInputButton)
+        val container = findViewById<LinearLayout>(R.id.container)
+
+        textInputButton.setOnClickListener {
+            val textItem = layoutInflater.inflate(R.layout.text_item, null)
+            val editText = textItem.findViewById<EditText>(R.id.editTextTextMultiLine)
+            val executeButton = textItem.findViewById<Button>(R.id.executeButton)
+            container.addView(textItem)
+
+            executeButton.setOnClickListener {
+                val text = editText.text.toString()
+                Log.d("MainActivity", "Texto: $text")
             }
         }
-    }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+        codeInputButton.setOnClickListener {
+            val codeItem = layoutInflater.inflate(R.layout.code_item, null)
+            val editText = codeItem.findViewById<EditText>(R.id.editTextTextMultiLine)
+            val executeButton = codeItem.findViewById<Button>(R.id.executeButton)
+            container.addView(codeItem)
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NotebookMobileTheme {
-        Greeting("Android")
+            executeButton.setOnClickListener {
+                val code = editText.text.toString()
+                Log.d("MainActivity", "Código: $code")
+            }
+        }
     }
 }
